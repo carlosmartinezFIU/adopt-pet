f7de6546ae798d524ed9c4bf2492fe137cbd9a49
carlosmartinezFIU <cmart566@fiu.edu>
2022-04-22 17:10:55 -0400

    Updated backend origin acces control

--- backend.js
+++ backend.js
@@ -4,12 +4,11 @@ const cors = require('cors')
 const fetch = require('node-fetch')
 require('dotenv').config()
 
-
-
-
-
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://carlosmartinezfiu.github.io']
+    
+}));
 
 
 /**

77576e56335e0cffd57665d2f9002219272be6b3
carlosmartinezFIU <cmart566@fiu.edu>
2022-04-24 18:12:39 -0400

    update

--- server.js
+++ server.js
@@ -45,6 +45,9 @@ app.get("/data", async (req, res) =>{
 
   if(process.env.NODE_ENV === 'production'){
       app.use(express.static('build'));
+      app.get('*', (req, res) => {
+          req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+      })
   }
 
 /**
